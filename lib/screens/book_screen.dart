a4ddedc98932b711d745ad508720c8aede81f7c4
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2022-05-28 16:29:15 -0300

    Changed BookArguments

--- lib/screens/book_screen.dart
+++ lib/screens/book_screen.dart
@@ -19,7 +19,7 @@ class BookArguments {
   final int? id;
   final String url;
   final String name;
-  final String imageURL;
+  final String? imageURL;
   final String? imageURL2;
   final Providers provider;
 
@@ -27,7 +27,7 @@ class BookArguments {
     required this.url,
     required this.name,
     required this.provider,
-    required this.imageURL,
+    this.imageURL,
     this.imageURL2,
     this.id,
   });
